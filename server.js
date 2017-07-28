6d723a91c6018fa5cca86d8f9469234d33367f5c
zillahpoliks8 <poliquitzillahit1b@gmail.com>
2017-07-28 17:26:30 +0800

    pusher

--- server.js
+++ server.js
@@ -1,45 +1,46 @@
-
+//require modules
 var express = require('express');
 var path = require('path');
-
+//instantiate express
 var app = express();
-
+//set port
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/apage.html'));
+app.get('/apage', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/apage.html'));
 });
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/MOCK_DATA.htm'));
+app.get('/MOCK_DATA', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/MOCK_DATA.htm'));
 });
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/regis.html'));
+app.get('/regis', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/regis.html'));
 });
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/profile.html'));
+app.get('/profile', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/profile.html'));
 });
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/contact.php'));
+app.get('/contact', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/contact.php'));
 });
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/services.html'));
+app.get('/services', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/services.html'));
 });
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/login.html'));
+app.get('/login', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/login.html'));
 });
 
+//express server listen
 var server = app.listen(app.get('port'), function(){
-  console.log('Server listening on port ',app.get('port'));
-});
+  console.log('Server listening on port 5000 ',app.get('port'));
+ });
